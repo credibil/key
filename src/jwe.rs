8487a5e46900e99c3944c88b15e20af06c4d1f51
Andrew Goldie <goldie@worksend.com>
2025-04-29 15:50:11 +1200

    move algorithm enums to ose

--- src/jwe.rs
+++ src/jwe.rs
@@ -52,7 +52,7 @@ mod encrypt;
 
 use anyhow::{Result, bail};
 use base64ct::{Base64UrlUnpadded, Encoding};
-use credibil_ose::{PublicKey, Receiver};
+use credibil_ose::{AlgAlgorithm, EncAlgorithm, PublicKey, Receiver};
 use encrypt::JweBuilder;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
@@ -236,52 +236,6 @@ pub struct Header {
     /// algorithm is used to encrypt the CEK.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub tag: Option<String>,
-}
-
-/// The algorithm used to perform authenticated content encryption. That is,
-/// encrypting the plaintext to produce the ciphertext and the Authentication
-/// Tag. MUST be an AEAD algorithm.
-#[derive(Clone, Debug, Default, Deserialize, Serialize, PartialEq, Eq)]
-pub enum EncAlgorithm {
-    /// AES GCM using a 256-bit key.
-    #[default]
-    #[serde(rename = "A256GCM")]
-    A256Gcm,
-
-    /// XChaCha20-Poly1305 is a competitive alternative to AES-256-GCM because
-    /// it’s fast and constant-time without hardware acceleration (resistent
-    /// to cache-timing attacks). It also has longer nonce length to alleviate
-    /// the risk of birthday attacks when nonces are generated randomly.
-    #[serde(rename = "XChacha20+Poly1305")]
-    XChaCha20Poly1305,
-}
-
-/// The algorithm used to encrypt (key encryption) or derive (key agreement)
-/// the value of the shared content encryption key (CEK).
-#[derive(Clone, Debug, Default, Deserialize, Serialize, PartialEq, Eq)]
-pub enum AlgAlgorithm {
-    /// Elliptic Curve Diffie-Hellman Ephemeral-Static key agreement using
-    /// Concat KDF.
-    ///
-    /// Uses Direct Key Agreement — a key agreement algorithm is used to agree
-    /// upon the CEK value.
-    #[default]
-    #[serde(rename = "ECDH-ES")]
-    EcdhEs,
-
-    /// ECDH-ES using Concat KDF and CEK wrapped with "A256KW".
-    ///
-    /// Uses Key Agreement with Key Wrapping — a Key Management Mode in which
-    /// a key agreement algorithm is used to agree upon a symmetric key used
-    /// to encrypt the CEK value to the intended recipient using a symmetric
-    /// key wrapping algorithm.
-    #[serde(rename = "ECDH-ES+A256KW")]
-    EcdhEsA256Kw,
-
-    /// Elliptic Curve Integrated Encryption Scheme for secp256k1.
-    /// Uses AES 256 GCM and HKDF-SHA256.
-    #[serde(rename = "ECIES-ES256K")]
-    EciesEs256K,
 }
 
 /// The compression algorithm applied to the plaintext before encryption.
